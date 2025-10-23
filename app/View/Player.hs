8d0f076cf678bb4d2df7feb9ba4fef30cf0e2b35
MauritsWilke <mauritswilke@gmail.com>
2025-10-23 13:54:14 +0200

    fix: actually refactor player view function'

--- app/View/Player.hs
+++ app/View/Player.hs
@@ -6,7 +6,10 @@ import Utils.Board
 
 positionPlayer :: Int -> Int -> Int -> Int -> Picture -> Picture
 positionPlayer x y width height =
-  Translate (fromIntegral x - (0.5 * fromIntegral width)) (fromIntegral y - (0.5 * fromIntegral height))  
+  Translate (dx * tileWidth + halfTile) (dy * tileWidth + halfTile)
+  where
+    dx = fromIntegral x - (0.5 * fromIntegral width)
+    dy = fromIntegral y - (0.5 * fromIntegral height)
 
 drawPlayer :: Board -> Player -> Picture
 drawPlayer _ NoPlayer   = blank
@@ -20,7 +23,8 @@ drawPlayerDebug _ _ NoPlayer =
   Color red $ Text "there is no current level"
 drawPlayerDebug i Board{..} Player{..}
   | i == 2 = let (x, y) = tilePosition in
-    positionPlayer x y width height
+    Translate (-halfTile) halfTile
+      $ positionPlayer x y width height
       $ Scale 0.08 0.08
       $ Color red
       $ Text ("(" ++ show x ++ ", " ++ show y ++ ")")
