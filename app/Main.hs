c6277c90894cd594df95766a1fa70a0e492c93d0
MauritsWilke <mauritswilke@gmail.com>
2025-10-28 12:03:04 +0100

    refactor: remove redundant comment

--- app/Main.hs
+++ app/Main.hs
@@ -13,12 +13,3 @@ main = playIO (InWindow "Pac-Man" (400, 400) (0, 0))
        view
        input
        step
--- main = playIO (InWindow "Pac-Man" (400, 400) (0, 0))
---               black            -- Background color
---               30               -- Frames per second
---               initialState     -- Initial state
---               view             -- View function
---               input            -- Event function
---               step             -- Step function
-
--- main = display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
