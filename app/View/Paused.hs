9b4e3cf9e90a48ef17d34496cb7132390e691cda
MauritsWilke <mauritswilke@gmail.com>
2025-11-07 20:47:44 +0100

    feat: add extra information to the pause menu

--- app/View/Paused.hs
+++ app/View/Paused.hs
@@ -11,4 +11,14 @@ drawPaused tw Board{..} p
     : zipWith (\i -> let j = i * (tw / 20) in Translate ((-7.5) * tw - j) j) 
       [0..] -- Gloss does not have bold text, so I'll do it myself
       (replicate 10 (Color white $ Scale (tw / 32) (tw / 32) $ Text "PAUSED"))
+    ++ 
+    [ Translate ((-7.6125) * tw) (- (tw * 2)) 
+      $ Color white 
+      $ Scale (tw / 128) (tw / 128) 
+      $ Text "press 'p' to continue playing"
+    , Translate ((-7.35) * tw) (- (tw * 4)) 
+      $ Color white 
+      $ Scale (tw / 128) (tw / 128) 
+      $ Text "press 'esc' to exit the game"
+    ]
   | otherwise = blank
