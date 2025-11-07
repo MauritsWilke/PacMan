1b46642c391d7a6fdc27409658eb5684faab647d
MauritsWilke <mauritswilke@gmail.com>
2025-11-07 18:23:02 +0100

    feat: add arrow to visualise queued movement

--- app/View/Player.hs
+++ app/View/Player.hs
@@ -10,12 +10,31 @@ positionPlayer tw (x,y) width height =
     dx = x - (0.5 * fromIntegral width)
     dy = y - (0.5 * fromIntegral height)
 
+positionArrow :: Float -> Direction -> Picture -> Picture
+positionArrow tw dir = Translate dx dy . Rotate rot
+  where
+    offset = 0.7 * tw
+    (dx, dy, rot) = case dir of
+      West  -> (-offset, 0, 270)
+      East  -> ( offset, 0, 90)
+      South -> (0, -offset, 180)
+      North -> (0,  offset, 0)
+
 drawPlayer :: Float -> Board -> Player -> Picture
 drawPlayer _ _ NoPlayer   = blank
-drawPlayer l Board{..} Player{..} = let (x, y) = position in
-  positionPlayer l (y,x) width height
-    $ Color yellow
-    $ circleSolid (0.5 * l)
+drawPlayer l Board{..} Player{..} =
+  let
+    (x, y) = position
+  in Pictures
+    [ positionPlayer l (y,x) width height
+        $ Color yellow
+        $ circleSolid (0.5 * l)
+    , positionPlayer l (y,x) width height
+        $ Color red
+        $ positionArrow l queuedDir
+        $ Translate (- arrowSize) 0
+        $ polygon [(0,0), (arrowSize, arrowSize), (2 * arrowSize, 0)]
+    ] where arrowSize = 7
 
 drawPlayerDebug :: Float -> Int -> Board -> Player -> Picture
 drawPlayerDebug _ _ _ NoPlayer =
